8c1651b07864b1b126146197fca7bbf445757692
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-03 14:53:10 +0500

    profile

--- pages/profile.js
+++ pages/profile.js
@@ -1,4 +1,4 @@
-import { getSession } from 'next-auth/client';
+// import { getSession } from 'next-auth/client';
 
 import UserProfile from '../components/profile/user-profile';
 
@@ -6,21 +6,21 @@ function ProfilePage() {
   return <UserProfile />;
 }
 
-export async function getServerSideProps(context) {
-  const session = await getSession({ req: context.req });
+// export async function getServerSideProps(context) {
+//   const session = await getSession({ req: context.req });
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/auth',
-        permanent: false,
-      },
-    };
-  }
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: '/auth',
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  return {
-    props: { session },
-  };
-}
+//   return {
+//     props: { session },
+//   };
+// }
 
 export default ProfilePage;
