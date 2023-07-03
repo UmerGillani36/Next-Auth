7b403dab5b965e6bc71db5d15d9e631b71be2ab1
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-03 18:25:17 +0500

    sinup api

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,49 +1,50 @@
-// import { hashPassword } from '../../../lib/auth';
-// import { connectToDatabase } from '../../../lib/db';
+import { hashPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
 
-// async function handler(req, res) {
-//   if (req.method !== 'POST') {
-//     return;
-//   }
+async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return;
+  }
 
-//   const data = req.body;
+  const data = req.body;
 
-//   const { email, password } = data;
+  const { email, password } = data;
+  console.log('email', email);
+  console.log('password', password);
+  if (
+    !email ||
+    !email.includes('@') ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message:
+        'Invalid input - password should also be at least 7 characters long.',
+    });
+    return;
+  }
 
-//   if (
-//     !email ||
-//     !email.includes('@') ||
-//     !password ||
-//     password.trim().length < 7
-//   ) {
-//     res.status(422).json({
-//       message:
-//         'Invalid input - password should also be at least 7 characters long.',
-//     });
-//     return;
-//   }
+  const client = await connectToDatabase();
 
-//   const client = await connectToDatabase();
+  const db = client.db();
+  console.log('db', db);
+  const existingUser = await db.collection('users').findOne({ email: email });
 
-//   const db = client.db();
+  if (existingUser) {
+    res.status(422).json({ message: 'User exists already!' });
+    client.close();
+    return;
+  }
 
-//   const existingUser = await db.collection('users').findOne({ email: email });
+  const hashedPassword = await hashPassword(password);
 
-//   if (existingUser) {
-//     res.status(422).json({ message: 'User exists already!' });
-//     client.close();
-//     return;
-//   }
+  const result = await db.collection('users').insertOne({
+    email: email,
+    password: hashedPassword,
+  });
 
-//   const hashedPassword = await hashPassword(password);
+  res.status(201).json({ message: 'Created user!' });
+  client.close();
+}
 
-//   const result = await db.collection('users').insertOne({
-//     email: email,
-//     password: hashedPassword,
-//   });
-
-//   res.status(201).json({ message: 'Created user!' });
-//   client.close();
-// }
-
-// export default handler;
+export default handler;
