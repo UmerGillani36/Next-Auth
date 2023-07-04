146268b21ee8b1415916dc3025e1864ff6c12908
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-04 11:49:45 +0500

    wrapp with session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
-// import { Provider } from 'next-auth/client';
+import { SessionProvider } from 'next-auth/react';
 
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    // <Provider session={pageProps.session}>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-    // </Provider>
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
 
