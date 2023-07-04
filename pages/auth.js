3c63c93513903de57e0cc973c2c9c3ecf479c493
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-04 16:14:53 +0500

    auth

--- pages/auth.js
+++ pages/auth.js
@@ -1,26 +1,26 @@
-// import { useRouter } from 'next/router';
-// import { getSession } from 'next-auth/client';
-// import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { getSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 
 import AuthForm from '../components/auth/auth-form';
 
 function AuthPage() {
-  // const [isLoading, setIsLoading] = useState(true);
-  // const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   getSession().then((session) => {
-  //     if (session) {
-  //       router.replace('/');
-  //     } else {
-  //       setIsLoading(false);
-  //     }
-  //   });
-  // }, [router]);
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace('/');
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
 
-  // if (isLoading) {
-  //   return <p>Loading...</p>;
-  // }
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return <AuthForm />;
 }
