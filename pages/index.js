7a77c1e6b4e749a9576838d63c6b34975e2ae4ba
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-04 11:50:16 +0500

    getting sessions

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import StartingPageContent from '../components/starting-page/starting-page';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from './api/auth/[...nextauth]';
 
 function HomePage() {
   return <StartingPageContent />;
 }
-
+export async function getServerSideProps({ req, res }) {
+  return {
+    props: {
+      session: await getServerSession(req, res, authOptions),
+    },
+  };
+}
 export default HomePage;
