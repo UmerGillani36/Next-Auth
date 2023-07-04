ab6287c3a9bd9b5c2af0889f2eaab572a517f536
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-04 11:50:34 +0500

    route protection

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
-
+import { useSession } from 'next-auth/react';
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
+  const { data: session, status } = useSession();
+  console.log('session', session);
+  console.log('loading', status);
+
   return (
     <header className={classes.header}>
       <Link href="/">
@@ -12,15 +16,21 @@ function MainNavigation() {
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href="/auth">Login</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {status === 'unauthenticated' && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+          {status === 'authenticated' && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+          {status === 'authenticated' && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
