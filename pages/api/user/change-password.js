d0b9306bbf2eae825d1214a09b14c756477e7f63
UmerGillani36 <syedomershahgillani@gmail.com>
2023-07-04 16:15:06 +0500

    change password

--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,55 +1,54 @@
-// import { getSession } from 'next-auth/client';
+import { authOptions } from 'pages/api/auth/[...nextauth]';
+import { getServerSession } from 'next-auth/next';
+import { hashPassword, verifyPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
 
-// import { hashPassword, verifyPassword } from '../../../lib/auth';
-// import { connectToDatabase } from '../../../lib/db';
+async function handler(req, res) {
+  if (req.method !== 'PATCH') {
+    return;
+  }
 
-// async function handler(req, res) {
-//   if (req.method !== 'PATCH') {
-//     return;
-//   }
+  const session = await getServerSession(req, res, authOptions);
 
-//   const session = await getSession({ req: req });
+  if (!session) {
+    res.status(401).json({ message: 'Not authenticated!' });
+    return;
+  }
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
 
-//   if (!session) {
-//     res.status(401).json({ message: 'Not authenticated!' });
-//     return;
-//   }
+  const client = await connectToDatabase();
 
-//   const userEmail = session.user.email;
-//   const oldPassword = req.body.oldPassword;
-//   const newPassword = req.body.newPassword;
+  const usersCollection = client.db().collection('users');
 
-//   const client = await connectToDatabase();
+  const user = await usersCollection.findOne({ email: userEmail });
 
-//   const usersCollection = client.db().collection('users');
+  if (!user) {
+    res.status(404).json({ message: 'User not found.' });
+    client.close();
+    return;
+  }
 
-//   const user = await usersCollection.findOne({ email: userEmail });
+  const currentPassword = user.password;
 
-//   if (!user) {
-//     res.status(404).json({ message: 'User not found.' });
-//     client.close();
-//     return;
-//   }
+  const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
 
-//   const currentPassword = user.password;
+  if (!passwordsAreEqual) {
+    res.status(403).json({ message: 'Invalid password.' });
+    client.close();
+    return;
+  }
 
-//   const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
+  const hashedPassword = await hashPassword(newPassword);
 
-//   if (!passwordsAreEqual) {
-//     res.status(403).json({ message: 'Invalid password.' });
-//     client.close();
-//     return;
-//   }
+  const result = await usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hashedPassword } }
+  );
 
-//   const hashedPassword = await hashPassword(newPassword);
+  client.close();
+  res.status(200).json({ message: 'Password updated!' });
+}
 
-//   const result = await usersCollection.updateOne(
-//     { email: userEmail },
-//     { $set: { password: hashedPassword } }
-//   );
-
-//   client.close();
-//   res.status(200).json({ message: 'Password updated!' });
-// }
-
-// export default handler;
+export default handler;
